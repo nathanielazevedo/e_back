4d674e88ac7ef290d56615e93fb215f67c925e31
nateazevedo <nathanielazevedo@hotmail.com>
2023-07-15 22:07:25 -0700

    dog

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<p>Hello, Dog!</p>"
 
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0',port=8080)
